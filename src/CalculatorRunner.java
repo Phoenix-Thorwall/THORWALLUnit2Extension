658f1afd07f54716a5222b9bf8bfbe47947f5a32
Phoenix Thorwall <phoenixt3@nycstudents.net>
2022-10-25 13:19:32 -0400

    Initial commit

---
+++ src/CalculatorRunner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CalculatorRunner
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter an expression with *, /, +, -, % or ^: ");
+        
+    }
+}
